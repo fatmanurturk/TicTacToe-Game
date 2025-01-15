d696ab453aaac7295c4c4dc377ec86466c1b1bbb
Fatma Nur <141139125+fatmanurturk@users.noreply.github.com>
2025-01-15 10:56:24 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace tictactoee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OYUN BOYUTUNU SEÇİN ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3X3",
+            "5X5",
+            "7X7"});
+            this.comboBox1.Location = new System.Drawing.Point(44, 146);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(29, 233);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(178, 65);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "OYUNA BAŞLA ";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(313, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 552);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 613);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoee
+{
+    public partial class Form1 : Form
+    {
+        private bool OyuncuX = true; // Oyuncu değişimi için sınıf seviyesinde tanımlandı
+        private int ucSayisiX = 0; // X oyuncusu için üçlü sayacı
+        private int ucSayisiO = 0; // O oyuncusu için üçlü sayacı
+        private bool oyunBitti = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnStart.Click += btnStart_Click;
+            btnStart.Paint += btnStart_Paint;
+        }
+        private List<Tuple<Point, Point>> cizimler = new List<Tuple<Point, Point>>();
+
+        private void UzeriniCiz(Button btn1, Button btn2, Button btn3)
+        {
+            // Çizgi için bir kalem tanımlayın
+            Pen pen = new Pen(Color.Red, 4);
+
+            // İlk ve son butonun merkezlerini hesaplayın
+            Point startPoint = new Point(btn1.Left + btn1.Width / 2, btn1.Top + btn1.Height / 2);
+            Point endPoint = new Point(btn3.Left + btn3.Width / 2, btn3.Top + btn3.Height / 2);
+
+            // Panelin Graphics nesnesini kullanarak çizgi çizin
+            using (Graphics g = panel1.CreateGraphics())
+            {
+                g.DrawLine(pen, startPoint, endPoint);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (oyunBitti) return;
+            Button btn = sender as Button;
+            if (btn != null && btn.Text == "")
+            {
+                if (OyuncuX)
+                {
+                    btn.Text = "X";
+                }
+                else
+                {
+                    btn.Text = "O";
+                }
+                OyuncuX = !OyuncuX; // Oyuncu değişimi sağlanıyor
+
+                int boyut = Convert.ToInt32(comboBox1.SelectedItem.ToString().Substring(0, 1));
+                UcluKontrolu(boyut); // Her hamlede üçlü kontrolü yapılır
+
+                // Oyun bitimi kontrolü
+                TümKutularDolu(boyut); // Eğer tüm kutular dolarsa, kazananı belirle
+            }
+        }
+        // Daha önce sayılmış üçlülerin koordinatlarını depolamak için
+        private HashSet<string> sayilmisUcluler = new HashSet<string>();
+
+        private void UcluKontrolu(int boyut)
+        {
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < boyut - 2; j++)
+                {
+                    // Yatay üçlü kontrolü
+                    if (((Button)panel1.Controls[$"btn_{i}_{j}"]).Text != "" &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i}_{j + 1}"]).Text &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i}_{j + 2}"]).Text)
+                    {
+                        string ucluKoordinatlar = $"Yatay-{i}-{j}";
+                        if (!sayilmisUcluler.Contains(ucluKoordinatlar))
+                        {
+                            UcluSayisiniArttir(((Button)panel1.Controls[$"btn_{i}_{j}"]).Text);
+                            sayilmisUcluler.Add(ucluKoordinatlar);
+                            UzeriniCiz(
+                                (Button)panel1.Controls[$"btn_{i}_{j}"],
+                                (Button)panel1.Controls[$"btn_{i}_{j + 1}"],
+                                (Button)panel1.Controls[$"btn_{i}_{j + 2}"]
+                            );
+                        }
+                    }
+
+                    // Dikey üçlü kontrolü
+                    if (i < boyut - 2 &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text != "" &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i + 1}_{j}"]).Text &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i + 2}_{j}"]).Text)
+                    {
+                        string ucluKoordinatlar = $"Dikey-{i}-{j}";
+                        if (!sayilmisUcluler.Contains(ucluKoordinatlar))
+                        {
+                            UcluSayisiniArttir(((Button)panel1.Controls[$"btn_{i}_{j}"]).Text);
+                            sayilmisUcluler.Add(ucluKoordinatlar);
+                            UzeriniCiz(
+                                (Button)panel1.Controls[$"btn_{i}_{j}"],
+                                (Button)panel1.Controls[$"btn_{i + 1}_{j}"],
+                                (Button)panel1.Controls[$"btn_{i + 2}_{j}"]
+                            );
+                        }
+                    }
+
+                    // Çapraz üçlü kontrolü (sol üstten sağ alta)
+                    if (i < boyut - 2 && j < boyut - 2 &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text != "" &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i + 1}_{j + 1}"]).Text &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i + 2}_{j + 2}"]).Text)
+                    {
+                        string ucluKoordinatlar = $"Capraz1-{i}-{j}";
+                        if (!sayilmisUcluler.Contains(ucluKoordinatlar))
+                        {
+                            UcluSayisiniArttir(((Button)panel1.Controls[$"btn_{i}_{j}"]).Text);
+                            sayilmisUcluler.Add(ucluKoordinatlar);
+                            UzeriniCiz(
+                                (Button)panel1.Controls[$"btn_{i}_{j}"],
+                                (Button)panel1.Controls[$"btn_{i + 1}_{j + 1}"],
+                                (Button)panel1.Controls[$"btn_{i + 2}_{j + 2}"]
+                            );
+                        }
+                    }
+
+                    // Çapraz üçlü kontrolü (sağ üstten sol alta)
+                    if (i < boyut - 2 && j >= 2 &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text != "" &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i + 1}_{j - 1}"]).Text &&
+                        ((Button)panel1.Controls[$"btn_{i}_{j}"]).Text == ((Button)panel1.Controls[$"btn_{i + 2}_{j - 2}"]).Text)
+                    {
+                        string ucluKoordinatlar = $"Capraz2-{i}-{j}";
+                        if (!sayilmisUcluler.Contains(ucluKoordinatlar))
+                        {
+                            UcluSayisiniArttir(((Button)panel1.Controls[$"btn_{i}_{j}"]).Text);
+                            sayilmisUcluler.Add(ucluKoordinatlar);
+                            UzeriniCiz(
+                                (Button)panel1.Controls[$"btn_{i}_{j}"],
+                                (Button)panel1.Controls[$"btn_{i + 1}_{j - 1}"],
+                                (Button)panel1.Controls[$"btn_{i + 2}_{j - 2}"]
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void UcluSayisiniArttir(string oyuncu)
+        {
+            if (oyuncu == "X")
+            {
+                ucSayisiX++;
+            }
+            else if (oyuncu == "O")
+            {
+                ucSayisiO++;
+            }
+        }
+
+
+        // Oyun sonunda en çok üçlü yapan oyuncuyu belirleme
+        private void KazananBelirle()
+        {
+            if (ucSayisiX > ucSayisiO)
+            {
+                MessageBox.Show("1. Oyuncu kazandı! ");
+            }
+            else if (ucSayisiO > ucSayisiX)
+            {
+                MessageBox.Show("2. Oyuncu kazandı! ");
+            }
+            else
+            {
+                MessageBox.Show("Berabere! ");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Seçim değişikliği olduğunda yapılacak işlemleri buraya ekleyin
+        }
+
+        // Tüm kutular dolduğunda oyunun bitip bitmediğini kontrol et
+        private void TümKutularDolu(int boyut)
+        {
+            bool tumKutularDolu = true;
+
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < boyut; j++)
+                {
+                    Button btn = (Button)panel1.Controls[$"btn_{i}_{j}"];
+                    if (btn.Text == "") // Eğer bir kutu boşsa
+                    {
+                        tumKutularDolu = false;
+                        break;
+                    }
+                }
+                if (!tumKutularDolu)
+                    break;
+            }
+
+            if (tumKutularDolu) // Eğer tüm kutular dolduysa kazananı belirle
+            {
+                KazananBelirle();
+                oyunBitti = true;
+            }
+        }
+
+        private void StartGame()
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string secilenBoyut = comboBox1.SelectedItem.ToString();
+                int boyut;
+
+                if (secilenBoyut == "3X3")
+                {
+                    boyut = 3;
+                    MessageBox.Show("3x3 boyutunda oyun başlıyor ");
+                }
+                else if (secilenBoyut == "5X5")
+                {
+                    boyut = 5;
+                    MessageBox.Show("5x5 boyutunda oyun başlıyor ");
+                }
+                else if (secilenBoyut == "7X7")
+                {
+                    boyut = 7;
+                    MessageBox.Show("7x7 boyutunda oyun başlıyor ");
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz boyut!");
+                    return;
+                }
+
+                // Üçlü sayılarını sıfırla
+                ucSayisiX = 0;
+                ucSayisiO = 0;
+                oyunBitti = false;
+
+                // Paneli temizleyip butonları yerleştiriyoruz
+                panel1.Controls.Clear();
+                int butonBoyutu = 70;
+
+                for (int i = 0; i < boyut; i++)
+                {
+                    for (int j = 0; j < boyut; j++)
+                    {
+                        Button btn = new Button();
+                        btn.Width = butonBoyutu;
+                        btn.Height = butonBoyutu;
+
+                        btn.Left = j * butonBoyutu;
+                        btn.Top = i * butonBoyutu;
+
+                        btn.Name = $"btn_{i}_{j}";
+                        btn.Click += button1_Click;
+
+                        panel1.Controls.Add(btn);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen oyun boyutunu seçin!");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartGame(); // Başlat butonuna basıldığında oyunu başlatır
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 4);  // Çizgi rengini ve kalınlığını belirle
+            foreach (var cizim in cizimler)
+            {
+                e.Graphics.DrawLine(pen, cizim.Item1, cizim.Item2);
+            }
+        }
+
+        private void btnStart_Paint(object sender, PaintEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                Pen pen = new Pen(Color.Pink, 12);
+                e.Graphics.DrawRectangle(pen, 0, 0, btn.Width - 1, btn.Height - 1);
+            }
+        }
+    }
+}
